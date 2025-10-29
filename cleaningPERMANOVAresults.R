ed7a7e93ac0ff0f099c05e20feccd08f86d63aad
ianwinick <98334968+ianwinick@users.noreply.github.com>
2025-10-29 11:25:16 -0700

    added sev.year combinations looking within severities across years

--- cleaningPERMANOVAresults.R
+++ cleaningPERMANOVAresults.R
@@ -13,6 +13,25 @@ permResult <- results %>%
 
 write.csv(permResult, "PERMresults_repeatMeasure.csv")
 
+sevYear <- c("U.2020_vs_U.2021", "U.2020_vs_U.2022", "U.2020_vs_U.2023", "U.2020_vs_U.2024",
+             "U.2021_vs_U.2022", "U.2021_vs_U.2023", "U.2021_vs_U.2024",
+             "U.2022_vs_U.2023", "U.2022_vs_U.2024",
+             "U.2023_vs_U.2024",
+             "L.2020_vs_L.2021", "L.2020_vs_L.2022", "L.2020_vs_L.2023", "L.2020_vs_L.2024",
+             "L.2021_vs_L.2022", "L.2021_vs_L.2023", "L.2021_vs_L.2024",
+             "L.2022_vs_L.2023", "L.2022_vs_L.2024",
+             "L.2023_vs_L.2024",
+             "H.2020_vs_H.2021", "H.2020_vs_H.2022", "H.2020_vs_H.2023", "H.2020_vs_H.2024",
+             "H.2021_vs_H.2022", "H.2021_vs_H.2023", "H.2021_vs_H.2024",
+             "H.2022_vs_H.2023", "H.2022_vs_H.2024",
+             "H.2023_vs_H.2024")
+
+permResult <- results %>%
+  separate(element, into = c("sev.year", "element"), sep = ":") %>%
+  pivot_wider(names_from = element, values_from = x) %>%
+  filter(sev.year %in% sevYear)
+
+write.csv(permResult, "PERMresults_repeatMeasure_WITHINSEVERITY.csv")
 
 envfit <- read_csv("envfit_results.txt")
 write.csv(envfit, "envfit_results.csv")
