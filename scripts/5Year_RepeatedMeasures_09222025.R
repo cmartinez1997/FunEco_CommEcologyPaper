e998f611eef71a4a8398ceb8d8f99ef4fe09428f
Fern Bromley <fernb@arizona.edu>
2025-10-29 10:49:18 -0700

    envfit looks good

--- scripts/5Year_RepeatedMeasures_09222025.R
+++ scripts/5Year_RepeatedMeasures_09222025.R
@@ -137,134 +137,134 @@ ggsave("outputs/5yrNMDS.png", width = 7, height = 20, units = "in")
 
 
 
-
-
-
-# adjust centroids
-
-#wisconsin standardized cover
-
-# traits<-read.csv(file.choose(), header=T, row.names=1)#traits
-# traits<-log(traits[,1:3])#log transform (did we keep this in or nah?)
-
-
-str(cwm.traits)
-#cwm.traits <- as.numeric(cwm.traits$resprouting) #make resprouting a numeric value
-
-traits.dist<-vegdist(cwm.traits, method="euclidean")
-#cov.CLIFF<-cbind(cov.CLIFF, cwm.traits)#bind to mega dataframe...DO WE NEED THIS FOR TAX ANALYSIS
-
-str(cov.CLIFF)
-
-#This chunk of code controls how permutations are carried out and is essential
-#for the repeated mesures analysis
-CTRL.t <- how(within = Within(type = "free"), #restrict permutations for repeat measures
-              plots = Plots(type = "none"),
-              blocks=cov.CLIFF$plot,
-              nperm = 999,
-              observed = TRUE)
-
-# IMPORTANT #### As of 10/13, this was coded using cwm, but we took out the trait permanova, so i (ian) am switching it taxonomic
-adonis.out<-adonis2(cov.stand~plot+severity*Year, #treats time as split plot factor, plot as sample unit per Bakker 2024
-                    data=cov.CLIFF, # CAN SOMEONE CONFIRM THAT THIS IS CORRECT?
-                    method="bray", # changed from euclidean to bray
-                    permutations=CTRL.t,
-                    by="margin")
-
-adonis.out #interaction is significant, create new factor for pairwise adonis
-
-cov.CLIFF$sev.year<-paste(cov.CLIFF$severity,cov.CLIFF$Year)
-
-pairwise.out<-pairwise.adonis2(cov.stand~sev.year, #treats time as split plot factor, plot as sample unit per Bakker 2024
-                               data=cov.CLIFF, 
-                               method="bray", 
-                               by="margin")
-pairwise.out #multiple significant pairwise comparisons with bonferonni correction
-
-#Plot MDS of traits
-#extract points and centroids for each severity/year combo
-traits.mds<-metaMDS(traits.dist, weakties=F)
-traits.scores = data.frame(MDS1 = traits.mds$points[,1], MDS2 = traits.mds$points[,2],severity=cov.CLIFF$severity, Year=cov.CLIFF$Year)
-
-trait.centroids <- traits.scores %>%
-  group_by(severity,Year) %>%
-  dplyr::summarize(MDS1=mean(MDS1), MDS2=mean(MDS2))
-
-#traits in ENVfit
-trt.env<-envfit(traits.mds, cwm.traits, perm=999)
-trt.env<-as.data.frame(trt.env$vectors$arrows*sqrt(trt.env$vectors$r))
-trt.env$traits<-rownames(trt.env)
-
-
-#Graph it!  Creates ordinations for each year with all severities and overlays
-#envfit
-traits.scores %>%
-  ggplot(aes(x=MDS1, y=MDS2, linetype=severity, color=severity)) +
-  stat_ellipse(geom="polygon", aes(group=severity, fill=severity), alpha = 0.1) +
-  geom_point(shape=1, size=2) +
-  geom_point(data=trait.centroids, aes(MDS1, MDS2), size=1, shape=19) +
-  facet_wrap(~Year,ncol=1)+
-  scale_linetype_manual(values=c("solid", "dashed", "longdash"))+
-  scale_color_manual(values=c("firebrick", "darkorange3", "darkolivegreen4" ))+
-  scale_fill_manual(values=c("firebrick", "darkorange3", "darkolivegreen4" ))+
-  geom_segment(data=trt.env,aes(x=0,xend=NMDS1,y=0,yend=NMDS2),
-               arrow = arrow(length = unit(0.25, "cm")),colour="darkgrey", inherit.aes=FALSE) + 
-  geom_text(data=trt.env,aes(x=NMDS1,y=NMDS2,label=traits),size=3, inherit.aes=FALSE)+
-  theme_classic()
-
-
-trt.env
-
-#Graphs trat means by year and severity for each trait
-####Graph trait means
-cwms<-cov.CLIFF[, c(88,85:87)] 
-cwms[,2:4]<-exp(cwms[,2:4])
-cwms$sev.year<-as.factor(cwms$sev.year)
-detach(package:plyr)
-
-group.means<-cwms %>%
-  group_by(sev.year) %>%
-  summarize(mean.ldmc = mean(ldmc), SD.ldmc = sd(ldmc),
-     mean.sla = mean(sla), SD.sla = sd(sla),
-     mean.height=mean(height), SD.height=sd(height))
-group.means<-separate_wider_delim(group.means, cols = sev.year, delim = " ", names = c("severity", "year"))
-
-#########LDMC
-
-ldmc.bar<-ggplot(group.means) +
-  geom_bar( aes(x=severity, y=mean.ldmc),stat="identity", fill="skyblue", alpha=0.7) +
-  geom_errorbar( aes(x=severity, ymin=mean.ldmc-SD.ldmc, ymax=mean.ldmc+SD.ldmc), width=0.4, colour="black", alpha=0.9)+
-  facet_wrap(~year, ncol=1)+
-  labs(x = "Severity", y = bquote (LDMC~(gg^-1)))+
-  theme_classic()
-ldmc.bar
-
-ldmc.aov<-aov(ldmc~severity*Year, data=cov.CLIFF)
-emmeans(ldmc.aov, pairwise~severity|Year, adj=("mvt"))
-
-####SLA
-
-
-sla.bar<-ggplot(group.means) +
-  geom_bar( aes(x=severity, y=mean.sla),stat="identity", fill="skyblue", alpha=0.7) +
-  geom_errorbar( aes(x=severity, ymin=mean.sla-SD.sla, ymax=mean.sla+SD.sla), width=0.4, colour="black", alpha=0.9)+
-  facet_wrap(~year, ncol=1)+
-  labs(x = "Severity", y = bquote (log(SLA)))+
-  theme_classic()
-sla.bar
-
-sla.aov<-aov(sla~severity*Year, data=cov.CLIFF)
-emmeans(sla.aov, pairwise~severity|Year, adj=("mvt"))
-
-###########Height
-
-height.bar<-ggplot(group.means) +
-  geom_bar( aes(x=severity, y=mean.height),stat="identity", fill="skyblue", alpha=0.7) +
-  geom_errorbar( aes(x=severity, ymin=mean.height-SD.height, ymax=mean.height+SD.height), width=0.4, colour="black", alpha=0.9)+
-  facet_wrap(~year, ncol=1)+
-  labs(x = "Severity", y = bquote (log(Height)))+
-  theme_classic()
-height.bar
-
-height.aov<-aov(height~severity*Year, data=cov.CLIFF)
-emmeans(height.aov, pairwise~severity|Year, adj=("mvt"))
+# 
+# 
+# 
+# # adjust centroids
+# 
+# #wisconsin standardized cover
+# 
+# # traits<-read.csv(file.choose(), header=T, row.names=1)#traits
+# # traits<-log(traits[,1:3])#log transform (did we keep this in or nah?)
+# 
+# 
+# str(cwm.traits)
+# #cwm.traits <- as.numeric(cwm.traits$resprouting) #make resprouting a numeric value
+# 
+# traits.dist<-vegdist(cwm.traits, method="euclidean")
+# #cov.CLIFF<-cbind(cov.CLIFF, cwm.traits)#bind to mega dataframe...DO WE NEED THIS FOR TAX ANALYSIS
+# 
+# str(cov.CLIFF)
+# 
+# #This chunk of code controls how permutations are carried out and is essential
+# #for the repeated mesures analysis
+# CTRL.t <- how(within = Within(type = "free"), #restrict permutations for repeat measures
+#               plots = Plots(type = "none"),
+#               blocks=cov.CLIFF$plot,
+#               nperm = 999,
+#               observed = TRUE)
+# 
+# # IMPORTANT #### As of 10/13, this was coded using cwm, but we took out the trait permanova, so i (ian) am switching it taxonomic
+# adonis.out<-adonis2(cov.stand~plot+severity*Year, #treats time as split plot factor, plot as sample unit per Bakker 2024
+#                     data=cov.CLIFF, # CAN SOMEONE CONFIRM THAT THIS IS CORRECT?
+#                     method="bray", # changed from euclidean to bray
+#                     permutations=CTRL.t,
+#                     by="margin")
+# 
+# adonis.out #interaction is significant, create new factor for pairwise adonis
+# 
+# cov.CLIFF$sev.year<-paste(cov.CLIFF$severity,cov.CLIFF$Year)
+# 
+# pairwise.out<-pairwise.adonis2(cov.stand~sev.year, #treats time as split plot factor, plot as sample unit per Bakker 2024
+#                                data=cov.CLIFF, 
+#                                method="bray", 
+#                                by="margin")
+# pairwise.out #multiple significant pairwise comparisons with bonferonni correction
+# 
+# #Plot MDS of traits
+# #extract points and centroids for each severity/year combo
+# traits.mds<-metaMDS(traits.dist, weakties=F)
+# traits.scores = data.frame(MDS1 = traits.mds$points[,1], MDS2 = traits.mds$points[,2],severity=cov.CLIFF$severity, Year=cov.CLIFF$Year)
+# 
+# trait.centroids <- traits.scores %>%
+#   group_by(severity,Year) %>%
+#   dplyr::summarize(MDS1=mean(MDS1), MDS2=mean(MDS2))
+# 
+# #traits in ENVfit
+# trt.env<-envfit(traits.mds, cwm.traits, perm=999)
+# trt.env<-as.data.frame(trt.env$vectors$arrows*sqrt(trt.env$vectors$r))
+# trt.env$traits<-rownames(trt.env)
+# 
+# 
+# #Graph it!  Creates ordinations for each year with all severities and overlays
+# #envfit
+# traits.scores %>%
+#   ggplot(aes(x=MDS1, y=MDS2, linetype=severity, color=severity)) +
+#   stat_ellipse(geom="polygon", aes(group=severity, fill=severity), alpha = 0.1) +
+#   geom_point(shape=1, size=2) +
+#   geom_point(data=trait.centroids, aes(MDS1, MDS2), size=1, shape=19) +
+#   facet_wrap(~Year,ncol=1)+
+#   scale_linetype_manual(values=c("solid", "dashed", "longdash"))+
+#   scale_color_manual(values=c("firebrick", "darkorange3", "darkolivegreen4" ))+
+#   scale_fill_manual(values=c("firebrick", "darkorange3", "darkolivegreen4" ))+
+#   geom_segment(data=trt.env,aes(x=0,xend=NMDS1,y=0,yend=NMDS2),
+#                arrow = arrow(length = unit(0.25, "cm")),colour="darkgrey", inherit.aes=FALSE) + 
+#   geom_text(data=trt.env,aes(x=NMDS1,y=NMDS2,label=traits),size=3, inherit.aes=FALSE)+
+#   theme_classic()
+# 
+# 
+# trt.env
+# 
+# #Graphs trat means by year and severity for each trait
+# ####Graph trait means
+# cwms<-cov.CLIFF[, c(88,85:87)] 
+# cwms[,2:4]<-exp(cwms[,2:4])
+# cwms$sev.year<-as.factor(cwms$sev.year)
+# detach(package:plyr)
+# 
+# group.means<-cwms %>%
+#   group_by(sev.year) %>%
+#   summarize(mean.ldmc = mean(ldmc), SD.ldmc = sd(ldmc),
+#      mean.sla = mean(sla), SD.sla = sd(sla),
+#      mean.height=mean(height), SD.height=sd(height))
+# group.means<-separate_wider_delim(group.means, cols = sev.year, delim = " ", names = c("severity", "year"))
+# 
+# #########LDMC
+# 
+# ldmc.bar<-ggplot(group.means) +
+#   geom_bar( aes(x=severity, y=mean.ldmc),stat="identity", fill="skyblue", alpha=0.7) +
+#   geom_errorbar( aes(x=severity, ymin=mean.ldmc-SD.ldmc, ymax=mean.ldmc+SD.ldmc), width=0.4, colour="black", alpha=0.9)+
+#   facet_wrap(~year, ncol=1)+
+#   labs(x = "Severity", y = bquote (LDMC~(gg^-1)))+
+#   theme_classic()
+# ldmc.bar
+# 
+# ldmc.aov<-aov(ldmc~severity*Year, data=cov.CLIFF)
+# emmeans(ldmc.aov, pairwise~severity|Year, adj=("mvt"))
+# 
+# ####SLA
+# 
+# 
+# sla.bar<-ggplot(group.means) +
+#   geom_bar( aes(x=severity, y=mean.sla),stat="identity", fill="skyblue", alpha=0.7) +
+#   geom_errorbar( aes(x=severity, ymin=mean.sla-SD.sla, ymax=mean.sla+SD.sla), width=0.4, colour="black", alpha=0.9)+
+#   facet_wrap(~year, ncol=1)+
+#   labs(x = "Severity", y = bquote (log(SLA)))+
+#   theme_classic()
+# sla.bar
+# 
+# sla.aov<-aov(sla~severity*Year, data=cov.CLIFF)
+# emmeans(sla.aov, pairwise~severity|Year, adj=("mvt"))
+# 
+# ###########Height
+# 
+# height.bar<-ggplot(group.means) +
+#   geom_bar( aes(x=severity, y=mean.height),stat="identity", fill="skyblue", alpha=0.7) +
+#   geom_errorbar( aes(x=severity, ymin=mean.height-SD.height, ymax=mean.height+SD.height), width=0.4, colour="black", alpha=0.9)+
+#   facet_wrap(~year, ncol=1)+
+#   labs(x = "Severity", y = bquote (log(Height)))+
+#   theme_classic()
+# height.bar
+# 
+# height.aov<-aov(height~severity*Year, data=cov.CLIFF)
+# emmeans(height.aov, pairwise~severity|Year, adj=("mvt"))
